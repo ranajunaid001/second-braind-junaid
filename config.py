7f452a0b9c1da92e8c0a61e734cc072f8eccfa0c
ranajunaid001 <130623056+ranajunaid001@users.noreply.github.com>
2026-01-19 03:50:26 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+# Telegram
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+
+# OpenAI
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+# Google Sheets
+GOOGLE_SHEETS_CREDS = os.environ.get("GOOGLE_SHEETS_CREDS")
+SHEET_ID = os.environ.get("SHEET_ID")
